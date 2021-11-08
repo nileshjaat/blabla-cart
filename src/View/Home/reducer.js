3e1dafa5d322adc487be497411bc5c8a8c5a8dd8
Nilesh <nilesh21098@yahoo.com>
2021-11-08 13:50:34 +0530

    initialised home page with all files and also redux set up

---
+++ src/View/Home/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const HomeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default HomeReducer;
